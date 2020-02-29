c2f1e6c3060e6a8779da51532182a8014ff1e93e
RabeAlex <61452198+RabeAlex@users.noreply.github.com>
2020-02-29 17:46:52 +0300

    Update header.hpp

--- include/header.hpp
+++ include/header.hpp
@@ -3,4 +3,42 @@
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
 
+#include <iostream>
+
+template <typename T>
+class stack {
+    class Node {
+    public:
+        Node *pNext;
+        T data;
+
+        explicit Node(const T& value)
+                : data(value), pNext(nullptr)
+        {}
+
+        explicit Node(T&& value)
+                : data(std::move(value)), pNext(nullptr)
+        {}
+
+        template<typename... Args>
+        explicit Node(Args &&... value)
+                : data(std::forward<Args>(value)...), pNext(nullptr)
+        {}
+    };
+
+    Node *bottom;
+
+public:
+    stack();
+    ~stack();
+
+    template <typename ... Args>
+    void push_emplace(Args&&... value);
+    void push(const T& value);
+    void push(T&& value);
+    const T& head() const;
+    T pop();
+    int size();
+};
+
 #endif // INCLUDE_HEADER_HPP_
