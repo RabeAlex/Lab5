4e44efda1c9036ff19eaad14c8ef3f1e791f0420
RabeAlex <61452198+RabeAlex@users.noreply.github.com>
2020-02-29 20:13:01 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -5,23 +5,18 @@
 
 TEST(stack, Text) {
     stack<int> test;
-    int a = 2, b = 3, c = 4;
-
-    test.push(a);
-    test.push(b);
-    test.push(c);
-    test.push(12);
-    test.push_emplace(15);
-
-    EXPECT_EQ(std::is_move_assignable<stack<int>>::value, true);
+    
+    test.push_emplace(1);
+    test.push(2);
+    test.push_emplace(3);
     EXPECT_EQ(std::is_move_constructible<stack<int>>::value, true);
-
-    EXPECT_EQ(test.head(), 15);
-    EXPECT_EQ(test.pop(), 15);
-    EXPECT_EQ(test.pop(), 12);
-    EXPECT_EQ(test.pop(), 4);
+    EXPECT_EQ(std::is_move_assignable<stack<int>>::value, true);
+    EXPECT_EQ(test.head(), 3);
     EXPECT_EQ(test.pop(), 3);
+    EXPECT_EQ(test.head(), 2);
     EXPECT_EQ(test.pop(), 2);
+    EXPECT_EQ(test.head(), 1);
+    EXPECT_EQ(test.pop(), 1);
 }
 
 int main(int argc, char **argv) {
