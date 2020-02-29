892f301fd96a53032c392c389fd3eeed3de2ded2
RabeAlex <61452198+RabeAlex@users.noreply.github.com>
2020-02-29 18:59:22 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,27 +1,7 @@
 // Copyright 2018 Your Name <your_email>
 
 #include <gtest/gtest.h>
-#include "header.hpp"
 
-int main() {
-    stack<int> test;
-    int a = 2, b = 3, c = 4;
-
-    test.push(a);
-    test.push(b);
-    test.push(c);
-    test.push(12);
-    test.push_emplace(15);
-
-    std::cout << std::is_move_assignable<stack<int>>::value << std::endl;
-    std::cout << std::is_move_constructible<stack<int>>::value << std::endl;
-
-    std::cout << test.head() << std::endl;
-    std::cout << test.pop() << std::endl;
-    std::cout << test.pop() << std::endl;
-    std::cout << test.pop() << std::endl;
-    std::cout << test.pop() << std::endl;
-    std::cout << test.pop() << std::endl;
-
-    return 0;
+TEST(Example, EmptyTest) {
+    EXPECT_TRUE(true);
 }
